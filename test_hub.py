c38ee71b467092f7dbe8421ad3063eefbb5e9a39
wikeex <wikeexm@vip.qq.com>
2022-03-01 20:58:59 +0800

    commit

--- test_hub.py
+++ test_hub.py
@@ -4,11 +4,11 @@ from hub import check, Hub
 
 @pytest.mark.asyncio
 async def test_check():
-    result = await check('localhost', 8888)
+    result = await check(*('localhost', 8888))
     assert result is True
 
 
 @pytest.mark.asyncio
 async def test_run_server():
-    hub = Hub('localhost', 8888, [('localhost', 1080)])
+    hub = Hub('localhost', 8888, [('localhost', '1080')])
     await hub.run()
