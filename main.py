5e3c94958bf78f968f08da68249624eee9ddc849
wikeex <wikeexm@vip.qq.com>
2022-04-17 22:13:26 +0800

    update main

--- main.py
+++ main.py
@@ -9,24 +9,24 @@ from config import config
 
 async def main():
     proxies = [tuple(proxy.strip().split(':')) for proxy in config.get('hub', 'proxies').split(',')]
-    hub = Hub(
-        serv_host=config.get('hub', 'host'),
-        serv_port=int(config.get('hub', 'port')),
-        proxies=proxies
-    )
-
-    smoke = Smoke()
-    chia_monitor = ChiaMonitor(
-        ca_file=config.get('chia', 'ca_file'),
-        cert_file=config.get('chia', 'cert_file'),
-        key_file=config.get('chia', 'key_file'),
-        plots_quantity=int(config.get('chia', 'plots_quantity'))
-    )
-    nbminer_monitor = NBMinerMonitor(
-        host=config.get('nbminer', 'host'),
-        port=int(config.get('nbminer', 'port')),
-        alert_limit=float(config.get('nbminer', 'alert_limit'))
-    )
+    # hub = Hub(
+    #     serv_host=config.get('hub', 'host'),
+    #     serv_port=int(config.get('hub', 'port')),
+    #     proxies=proxies
+    # )
+    #
+    # smoke = Smoke()
+    # chia_monitor = ChiaMonitor(
+    #     ca_file=config.get('chia', 'ca_file'),
+    #     cert_file=config.get('chia', 'cert_file'),
+    #     key_file=config.get('chia', 'key_file'),
+    #     plots_quantity=int(config.get('chia', 'plots_quantity'))
+    # )
+    # nbminer_monitor = NBMinerMonitor(
+    #     host=config.get('nbminer', 'host'),
+    #     port=int(config.get('nbminer', 'port')),
+    #     alert_limit=float(config.get('nbminer', 'alert_limit'))
+    # )
     workers = [worker.strip() for worker in config.get('phala', 'workers').split(',')]
     prb_host = config.get('phala', 'prb_host')
     khala_node_host = config.get('phala', 'khala_node_host')
